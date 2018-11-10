2001db28c28f0e8f9b6f80ec5088c36c09dd5b11
kyeahen <bright19961@naver.com>
2018-11-10 14:47:00 +0900

    Create Seminar2

---
+++ Seminar2/iOS2_HW/iOS2_HW/ViewController/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOS2_HW
+//
+//  Created by 김예은 on 10/11/2018.
+//  Copyright © 2018 kyeahen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
